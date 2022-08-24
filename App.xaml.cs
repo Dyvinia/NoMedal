7ab6ffb7cf2559fa7844df89850bd4993b5b6297
Dyvine57 <dulana57@gmail.com>
2022-08-24 14:50:15 -0400

    initial commit

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,15 +1,52 @@
-﻿using System;
+﻿using DyviniaUtils;
+using DyviniaUtils.Dialogs;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace NoMedal {
+    public class ProgramConfig {
+        public string Path { get; set; }
+        public int Action { get; set; } = 0;
+    }
+
+    public class Config : SettingsManager<Config> {
+        public bool LaunchGame { get; set; } = false;
+        public List<ProgramConfig> Programs { get; set; } = new();
+    }
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+
+        public static readonly string Version = "v" + Assembly.GetExecutingAssembly().GetName().Version.ToString()[..5];
+        public static readonly string BaseDir = AppDomain.CurrentDomain.BaseDirectory;
+        public static readonly string AppName = Assembly.GetEntryAssembly().GetName().Name;
+
+        public static readonly FileInfo MedalPath = new(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Medal\\Medal.exe"));
+
+        public App() {
+            Config.Load();
+
+            Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            DispatcherUnhandledException += ExceptionDialog.UnhandledException;
+        }
+
+        protected override void OnStartup(StartupEventArgs e) {
+            new MainWindow().Show();
+
+            if (!MedalPath.Exists)
+                MessageBoxDialog.Show("Cannot locate Medal", AppName, MessageBoxButton.OK, DialogSound.Error);
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,133 @@
-﻿using System;
+﻿using DyviniaUtils.Dialogs;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NoMedal {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        public class Program {
+            public string Name { get; set; }
+            public ImageSource Icon { get; set; }
+
+            public string Path { get; set; }
+            public int Action { get; set; } = 0;
+        }
+        public ObservableCollection<Program> Programs = new();
+
         public MainWindow() {
             InitializeComponent();
+
+            ProgramListBox.ItemsSource = Programs;
+            LoadProgramsConfig();
+
+            Thread checkThread = new(CheckThread) { IsBackground = true };
+            checkThread.Start();
+        }
+
+        public void LoadProgramsConfig() {
+            Programs.Clear();
+            foreach (ProgramConfig pConfig in Config.Settings.Programs) {
+                string name = FileVersionInfo.GetVersionInfo(pConfig.Path).ProductName;
+                ImageSource iconImage = Icon;
+                try {
+                    Icon icon = System.Drawing.Icon.ExtractAssociatedIcon(pConfig.Path);
+                    iconImage = Imaging.CreateBitmapSourceFromHIcon(icon.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                }
+                catch {
+                    
+                }
+
+                Program program = new() { Name = name, Icon = iconImage, Path = pConfig.Path, Action = pConfig.Action };
+                Programs.Add(program);
+            }
+        }
+
+        
+        public void CheckThread() {
+            while (true) {
+                Process[] processes = Programs.SelectMany(p => Process.GetProcessesByName(Path.GetFileNameWithoutExtension(p.Path))).ToArray();
+
+                if (processes.Length != 0) {
+                    try {
+                        foreach (Process process in processes) {
+                            foreach (Process medalProcess in Process.GetProcessesByName("Medal"))
+                                medalProcess.Kill();
+                            process.WaitForExit();
+
+                            Process medal = new();
+                            medal.StartInfo.FileName = App.MedalPath.FullName;
+                            medal.StartInfo.WorkingDirectory = App.MedalPath.DirectoryName;
+                            medal.Start();
+                        }
+                    }
+                    catch (Exception ex) {
+                        Task.Run(() => {
+                            ExceptionDialog.Show(ex, App.AppName);
+                        });
+                    }
+                }
+                else Thread.Sleep(5000);
+            }
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e) {
+            base.OnKeyDown(e);
+
+            if (e.Key == Key.F12)
+                Process.Start("explorer.exe", $"/select, {Config.FilePath}");
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e) {
+            OpenFileDialog dialog = new() {
+                Title = "Select Program",
+                Filter = "Executable (*.exe) |*.exe",
+                FilterIndex = 2
+            };
+            if (dialog.ShowDialog() == true) {
+                if (!Config.Settings.Programs.Any(p => p.Path == dialog.FileName)) {
+                    Config.Settings.Programs.Add(new ProgramConfig() { Path = dialog.FileName, Action = 0 });
+                    Config.Save();
+                    LoadProgramsConfig();
+                }
+                else {
+                    MessageBoxDialog.Show("Program already exists", App.AppName, MessageBoxButton.OK, DialogSound.Error);
+                }
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e) {
+            Program program;
+            if (sender is Program)
+                program = sender as Program;
+            else
+                program = ((Button)sender).DataContext as Program;
+
+            MessageBoxResult result = MessageBoxDialog.Show("Remove Program from NoMedal?", App.AppName, MessageBoxButton.YesNo, DialogSound.Notify);
+            if (result == MessageBoxResult.Yes) {
+                ProgramConfig pConfig = Config.Settings.Programs.Find(p => p.Path == program.Path);
+                Config.Settings.Programs.Remove(pConfig);
+                Config.Save();
+                LoadProgramsConfig();
+            }
         }
     }
 }
